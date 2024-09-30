87af7570723a596d8eeb6cf73497140b18479f09
msz93v2 <junhao.zhang@mail.utoronto.ca>
2024-09-30 18:08:12 -0400

    added gettopgradebutton for task4

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -247,7 +247,7 @@ public class Application {
         // make a separate line.
         final JButton getAverageButton = new JButton("Get Average Grade");
         // TODO Task 4: Add another button for "Get Top Grade" (check the getAverageButton for example)
-
+        final JButton gettopgradeButton = new JButton("Get Top Grade");
         final JButton leaveTeamButton = new JButton("Leave Team");
         final JLabel resultLabel = new JLabel();
 
